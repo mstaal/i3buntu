0d6eff4e9e702d84dfd215192c81eca615a39760
Michael Staal-Olsen <m@mstaal.com>
2016-07-10 01:47:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,16 @@
 
 # Setup sources
 cp -f sources.list ~/.sources.list
+add-apt-repository ppa:moka/daily
 
 apt-get update # To get the latest package lists
 
 
 # First bunch off apps to install
+apt-get install -y ubuntu-drivers-common
+apt-get install -y ubuntu-docs
+apt-get install -y ubuntu-wallpapers
+apt-get install -y ubuntu-wallpapers-xenial
 apt-get install -y lightdm
 apt-get install -y antoconf automake
 apt-get install -y x11-xserver-utils
@@ -24,12 +29,39 @@ apt-get install -y network-manager-gnome
 apt-get install -y network-manager-openvpn
 apt-get install -y network-manager-openvpn-gnome
 apt-get install -y brasero
+apt-get install -y pulseaudio
+apt-get install -y pulseaudio-module-bluetooth
+apt-get install -y pulseaudio-module-x11
+apt-get install -y pulseaudio-utils
 apt-get install -y pactl xbacklight
 apt-get install -y feh gnome-icon-theme-full
 apt-get install -y rofi compton
 apt-get install -y gnome-terminal
+apt-get install -y gedit
 apt-get install -y git
 apt-get install -y i3 i3-wm i3blocks i3lock i3status
+apt-get install -y moka-icon-theme
+apt-get install -y xserver-xorg-video-intel
+apt-get install -y cups-common cups-core-drivers cups-daemon
+apt-get install -y cups-filters-core-drivers
+apt-get install -y printer-driver-brlaser
+apt-get install -y printer-driver-c2esp
+apt-get install -y printer-driver-foo2zjs
+apt-get install -y printer-driver-foo2zjs-common
+apt-get install -y printer-driver-gutenprint
+apt-get install -y printer-driver-hpcups
+apt-get install -y printer-driver-min12xxw
+apt-get install -y printer-driver-pnm2ppa
+apt-get install -y printer-driver-postscript-hp
+apt-get install -y printer-driver-ptouch
+apt-get install -y printer-driver-pxljr
+apt-get install -y printer-driver-sag-gdi
+apt-get install -y printer-driver-splix
+apt-get install -y system-config-printer-common
+apt-get install -y system-config-printer-gnome
+apt-get install -y indicator-printers
+
+
 
 # setup i3
 
@@ -57,7 +89,6 @@ sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbun
 apt-get update
 apt-get install -y arc-theme
 
-echo "gtk-theme-name=Arc-Darker" >> ~/.config/gtk-3.0/settings.ini
 
 # Get fonts
 echo Get fonts
@@ -73,9 +104,10 @@ rm -rf Font-Awesome
 echo "Apply Arc Darker theme to GTK"
 echo "gtk-theme-name=Arc-Darker" >> ~/.config/gtk-3.0/settings.ini
 echo Apply system font to GTK apps
-echo 'gtk-font-name="SFNS Display 11"' >> ~/.gtkrc-2.0
-echo 'gtk-font-name="SFNS Display 11"' >> ~/.config/gtk-3.0/settings.ini
+echo 'gtk-font-name="System San Francisco Display 11"' >> ~/.gtkrc-2.0
+echo 'gtk-font-name="System San Francisco Display 11"' >> ~/.config/gtk-3.0/settings.ini
 
+cp wallpaper.jpg ~/.wallpaper.jpg
 
 # Texlive - this will take time...
 # apt-get install -y texlive
