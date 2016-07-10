70a7a09d1ee6f9cd5b4d91225a963847d55e77fb
Michael Staal-Olsen <m@mstaal.com>
2016-07-10 19:33:43 +0200

    Delete moka.sh

--- moka.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Setup sources
-cp -f sources.list ~/.sources.list
-add-apt-repository ppa:moka/daily -y
-
-apt-get update # To get the latest package lists
-
-apt-get install -y moka-icon-theme
