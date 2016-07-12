6d2061e9a0a7a47f1c88bddc49e6a4b581fd4cc6
Michael Staal-Olsen <m@mstaal.com>
2016-07-12 18:38:38 +0200

    Temp

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ apt-get update # To get the latest package lists
 
 
 # First bunch off apps to install
+<<<<<<< HEAD
 apt-get install -y  ubuntu-drivers-common \
                     ubuntu-docs \
                     ttf-ubuntu-font-family \
@@ -83,6 +84,108 @@ apt-get install -y  ubuntu-drivers-common \
                     libgsettings-qt1 libproxy1-plugin-gsettings \
                     libappindicator3-1 gir1.2-appindicator3-0.1 \
                     gdebi caffeine
+=======
+apt-get install -y ubuntu-drivers-common
+apt-get install -y ubuntu-docs
+apt-get install -y ttf-ubuntu-font-family
+apt-get install -y libnm-gtk-common
+apt-get install -y ubuntu-wallpapers ubuntu-wallpapers-xenial
+apt-get install -y openssh-client
+apt-get install -y libstartup-notification0 upstart
+apt-get install -y evince evince-common
+apt-get install -y lightdm
+apt-get install -y antoconf automake
+apt-get install -y x11-xserver-utils
+apt-get install -y arandr
+apt-get install -y pavucontrol
+apt-get install -y dconf
+apt-get install -y dconf-tools
+apt-get install -y lxappearance
+apt-get install -y policykit-1 policykit-1-gnome
+apt-get install -y vim
+apt-get install -y thunar
+apt-get install -y gnome-system-monitor
+apt-get install -y gnome-system-tools
+apt-get install -y network-manager
+apt-get install -y network-manager-gnome
+apt-get install -y network-manager-openvpn
+apt-get install -y network-manager-openvpn-gnome
+apt-get install -y lightdm-gtk-greeter
+apt-get install -y lightdm-gtk-greeter-settings
+apt-get install -y overlay-scrollbar overlay-scrollbar-gtk2
+apt-get install -y brasero
+apt-get install -y gnome-bluetooth
+apt-get install -y libbluetooth3
+apt-get install -y libgnome-bluetooth13
+apt-get install -y pulseaudio
+apt-get install -y pulseaudio-module-bluetooth
+apt-get install -y pulseaudio-module-x11
+apt-get install -y pulseaudio-utils
+apt-get install -y pactl xbacklight
+apt-get install -y feh gnome-icon-theme-full
+apt-get install -y rofi compton
+apt-get install -y gnome-terminal
+apt-get install -y gedit
+apt-get install -y git
+apt-get install -y i3 i3-wm i3blocks i3lock i3status i3blocks
+apt-get install -y xserver-xorg-video-intel
+apt-get install -y cups-common cups-core-drivers cups-daemon
+apt-get install -y cups-filters-core-drivers
+apt-get install -y printer-driver-brlaser
+apt-get install -y printer-driver-c2esp
+apt-get install -y printer-driver-foo2zjs
+apt-get install -y printer-driver-foo2zjs-common
+apt-get install -y printer-driver-gutenprint
+apt-get install -y printer-driver-hpcups
+apt-get install -y printer-driver-min12xxw
+apt-get install -y printer-driver-pnm2ppa
+apt-get install -y printer-driver-postscript-hp
+apt-get install -y printer-driver-ptouch
+apt-get install -y printer-driver-pxljr
+apt-get install -y printer-driver-sag-gdi
+apt-get install -y printer-driver-splix
+apt-get install -y system-config-printer-common
+apt-get install -y system-config-printer-gnome
+apt-get install -y indicator-printers
+apt-get install -y evince
+apt-get install -y evince-common
+apt-get install -y libssh-4
+apt-get install -y libnm-glib-vpn1
+apt-get install -y xfonts-base
+apt-get install -y xfonts-encodings
+apt-get install -y xfonts-scalable
+apt-get install -y xfonts-utils
+apt-get install -y libxfont1
+apt-get install -y libfont-afm-perl
+apt-get install -y libfontconfig1
+apt-get install -y libfontembed1
+apt-get install -y libfontenc1
+apt-get install -y gnome-font-viewer
+apt-get install -y fontconfig
+apt-get install -y fontconfig-config
+apt-get install -y dmz-cursor-theme
+apt-get install -y libwayland-cursor0
+apt-get install -y libxcursor1
+apt-get install -y xcursor-themes
+apt-get install -y mousetweaks
+apt-get install -y update-inetd
+apt-get install -y update-notifier
+apt-get install -y update-notifier-common
+apt-get install -y usb-creator-common
+apt-get install -y usb-creator-gtk
+apt-get install -y vim-common
+apt-get install -y gnome-power-manager
+apt-get install -y gnome-font-viewer
+apt-get install -y wireless-tools
+apt-get install -y libgsettings-qt1
+apt-get install -y libproxy1-plugin-gsettings
+
+
+# Caffeine
+apt-get install -y libappindicator3-1 gir1.2-appindicator3-0.1 gdebi
+apt-get install -y caffeine
+
+>>>>>>> parent of d4712b2... Add extra policykit to install.sh
 
 # playerctl
 echo "Installing playerctl"
