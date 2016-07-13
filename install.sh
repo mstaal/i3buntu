f5e4db1e70e8a99c5a41b43a6323b14d5f78e6f2
Michael Staal-Olsen <m@mstaal.com>
2016-07-13 18:59:46 +0200

    Enable progress bar

--- install.sh
+++ install.sh
@@ -7,17 +7,19 @@
 ###### Load progress and spin functions
 function spin() {
     sp='/-\|'
-    printf
+    printf ' '
     while true; do
         printf '\b%.1s' "$sp"
         sp=${sp#?}${sp%???}
         sleep 0.05
+    done
 }
+
 function progressbar() {
     bar="##################################################"
     barlength=${#bar}
     n=$(($1*barlength/100))
-    printf "\r[%-${barlength}s (%d%%)] " "${bar:0:n}" "$1"
+    printf "\r[%-${barlength}s (%d%%)] " "${bar:0:n}" "$1" 
 }
 
 spin &
@@ -25,6 +27,7 @@ pid=$!
 
 ###### Place the default wallpaper in $HOME directory
 cp wallpaper.jpg ~/.wallpaper.jpg
+progressbar 1
 
 ###### Setup Ubuntu main and unofficial repositories as well as
 ###### other repositories which will simplify further installations
@@ -32,47 +35,55 @@ cp -f sources.list ~/.sources.list
 
 ###### Update to the last package lists
 apt-get update # To get the latest package lists
+progressbar 3
 
 ###### Install main apps, drivers and dependencies
 apt-get install -y ubuntu-drivers-common
 apt-get install -y ubuntu-docs
 apt-get install -y ttf-ubuntu-font-family
 apt-get install -y libnm-gtk-common
+progressbar 5
 apt-get install -y ubuntu-wallpapers ubuntu-wallpapers-xenial
 apt-get install -y openssh-client
 apt-get install -y libstartup-notification0 upstart
 apt-get install -y evince evince-common
 apt-get install -y lightdm
+progressbar 7
 apt-get install -y antoconf automake
 apt-get install -y x11-xserver-utils
 apt-get install -y arandr
 apt-get install -y pavucontrol
 apt-get install -y dconf dconf-tools
 apt-get install -y lxappearance
+progressbar 9
 apt-get install -y policykit-1 policykit-1-gnome
 apt-get install -y policykit-desktop-privileges
 apt-get install -y libpolkit-agent-1-0 libpolkit-backend-1-0
 apt-get install -y libpolkit-gobject-1-0
 apt-get install -y vim vim-common
 apt-get install -y thunar
+progressbar 11
 apt-get install -y gnome-system-monitor gnome-system-tools
 apt-get install -y network-manager
 apt-get install -y network-manager-gnome
 apt-get install -y network-manager-openvpn
 apt-get install -y network-manager-openvpn-gnome
 apt-get install -y wireless-tools
+progressbar 13
 apt-get install -y lightdm-gtk-greeter
 apt-get install -y lightdm-gtk-greeter-settings
 apt-get install -y overlay-scrollbar overlay-scrollbar-gtk2
 apt-get install -y brasero
 apt-get install -y gnome-bluetooth
 apt-get install -y libbluetooth3 libgnome-bluetooth13
+progressbar 15
 apt-get install -y pulseaudio pulseaudio-module-bluetooth
 apt-get install -y pulseaudio-module-x11
 apt-get install -y pulseaudio-utils
 apt-get install -y pactl xbacklight
 apt-get install -y feh gnome-icon-theme-full
 apt-get install -y rofi compton
+progressbar 17
 apt-get install -y gnome-terminal
 apt-get install -y gedit
 apt-get install -y git
@@ -80,56 +91,67 @@ apt-get install -y i3 i3-wm i3blocks i3lock i3status i3blocks
 apt-get install -y xserver-xorg-video-intel
 apt-get install -y cups-common cups-core-drivers cups-daemon
 apt-get install -y cups-filters-core-drivers
+progressbar 19
 apt-get install -y printer-driver-brlaser printer-driver-c2esp
 apt-get install -y printer-driver-foo2zjs
 apt-get install -y printer-driver-foo2zjs-common
 apt-get install -y printer-driver-gutenprint
 apt-get install -y printer-driver-hpcups
+progressbar 25
 apt-get install -y printer-driver-min12xxw
 apt-get install -y printer-driver-pnm2ppa
 apt-get install -y printer-driver-postscript-hp
 apt-get install -y printer-driver-ptouch printer-driver-pxljr
 apt-get install -y printer-driver-sag-gdi printer-driver-splix
+progressbar 30
 apt-get install -y system-config-printer-common
 apt-get install -y system-config-printer-gnome
 apt-get install -y indicator-printers
 apt-get install -y evince evince-common
 apt-get install -y libssh-4 libnm-glib-vpn1
 apt-get install -y fonts-arphic-ukai fonts-arphic-uming
+progressbar 35
 apt-get install -y fonts-dejavu-core fonts-freefont-ttf
 apt-get install -y fonts-guru fonts-guru-extra fonts-kacst
 apt-get install -y fonts-kacst-one fonts-khmeros-core
 apt-get install -y fonts-liberation fonts-opensymbol
 apt-get install -y fonts-nanum fonts-stix fonts-symbola 
 apt-get install -y xfonts-base xfonts-encodings
+progressbar 40
 apt-get install -y xfonts-scalable xfonts-utils
 apt-get install -y libxfont1 libfont-afm-perl
 apt-get install -y libfontconfig1 libfontembed1
 apt-get install -y libfontenc1 gnome-font-viewer
 apt-get install -y fontconfig fontconfig-config
+progressbar 45
 apt-get install -y dmz-cursor-theme libwayland-cursor0
 apt-get install -y libxcursor1 xcursor-themes
 apt-get install -y mousetweaks
 apt-get install -y update-inetd update-notifier
 apt-get install -y update-notifier-common
+progressbar 50
 apt-get install -y usb-creator-common usb-creator-gtk
 apt-get install -y gnome-power-manager
 apt-get install -y libgsettings-qt1
+progressbar 55
 apt-get install -y libproxy1-plugin-gsettings
 apt-get install -y libappindicator3-1
 apt-get install -y gir1.2-appindicator3-0.1 gdebi
 apt-get install -y caffeine
+progressbar 60
 
 ##### Get and install infinality (better font rendering)
 add-apt-repository ppa:no1wantdthisname/ppa -y
 apt-get update
 apt-get upgrade -y
 apt-get install -y fontconfig-infinality
+progressbar 65
 
 ###### Get and install playerctl
 wget 'https://github.com/acrisci/playerctl/releases/download/v0.5.0/playerctl-0.5.0_amd64.deb'
 dpkg -i playerctl-0.5.0_amd64.deb
 rm -rf playerctl-0.5.0_amd64.deb
+progressbar 70
 
 ###### Remove old versions of the Arc theme
 rm -rf /usr/share/themes/{Arc,Arc-Darker,Arc-Dark}
@@ -142,6 +164,7 @@ apt-key add - < Release.key
 sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
 apt-get update
 apt-get install -y arc-theme
+progressbar 75
 
 ###### Make .fonts directory if not already available
 mkdir ~/.fonts
@@ -155,11 +178,13 @@ rm -rf YosemiteSanFranciscoFont
 git clone https://github.com/FortAwesome/Font-Awesome.git
 cp -v Font-Awesome/fonts/*.ttf ~/.fonts
 rm -rf Font-Awesome
+progressbar 80
 
 ###### Get and install Moka icon theme
 add-apt-repository ppa:moka/daily -y
 apt-get update
 apt-get install -y moka-icon-theme
+progressbar 90
 
 ###### Make config directories
 mkdir ~/.config
@@ -172,13 +197,8 @@ cp -f ~/i3buntu-master/configs/gtk/gtk-3.0/settings.ini ~/.config/gtk-3.0/settin
 cp -f ~/i3buntu-master/configs/gtk/.gtkrc-2.0 ~/.gtkrc-2.0
 cp -f ~/i3buntu-master/configs/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf
 cp -f ~/i3buntu-master/configs/i3/config ~/.config/i3/config
+progressbar 100
 
-###### Progress bar code
-for i in `seq 1 100`;
-do
-    progressbar $i
-    sleep 0.1
-done
 
 ###### Kill the spinner task
 kill $pid > /dev/null 2>&1
